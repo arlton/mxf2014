a0a2073cc5dae04e8cae3d9d2f7e37578c20c454
David Hudson <im@davidhudson.me>
2014-02-01 00:05:12 -0600

    Successfully setup mongo database for registrants

--- web.js
+++ web.js
@@ -1,13 +1,26 @@
 // web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var express, logfmt, mongoose, port, app;
+
+express  = require("express");
+logfmt   = require("logfmt");
+mongoose = require ("mongoose");
+_        = require("underscore");
+
+port     = Number(process.env.PORT || 5000);
+app      = express();
 
 app.use(logfmt.requestLogger());
 
 app.use(express.static('public/'));
 
-var port = Number(process.env.PORT || 5000);
+mongoose.connect(process.env.MONGOHQ_URL, function (err, res) {
+  if (err) {
+    console.log ('DB Error: ' + process.env.MONGOHQ_URL + '. ' + err);
+  } else {
+    console.log ('DB Connected: ' + process.env.MONGOHQ_URL);
+  }
+});
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
