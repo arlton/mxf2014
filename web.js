3d56f8470d69786d218c1954d454fd8e487cdb2b
David Hudson <im@davidhudson.me>
2014-01-31 22:54:17 -0600

    Trying again. FUUUCK

--- web.js
+++ web.js
@@ -6,6 +6,9 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.use(express.static('public/'));
+app.use(express.static('public/assets'));
+app.use(express.static('public/assets/js'));
+app.use(express.static('public/assets/css'));
 
 var port = Number(process.env.PORT || 80);
 app.listen(port, function() {
