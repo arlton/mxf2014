ad7bd7e090fd77e892e0bd3b77049177ee722802
David Hudson <im@davidhudson.me>
2014-02-05 21:23:47 -0600

    Wtf

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 // web.js
-var express, logfmt, mongoose, port, app;
+var express, logfmt, mongoose, port, app, stripe;
 
 express  = require("express");
 logfmt   = require("logfmt");
-mongoose = require ("mongoose");
+mongoose = require("mongoose");
 _        = require("underscore");
+stripe   = require("stripe")(process.env.STRIPE_API_KEY);;
 
 port     = Number(process.env.PORT || 5000);
 app      = express();
@@ -12,6 +13,8 @@ app      = express();
 app.use(logfmt.requestLogger());
 
 app.use(express.static('public/'));
+
+app.use('/', require('./app/routes'));
 
 mongoose.connect(process.env.MONGOHQ_URL, function (err, res) {
   if (err) {
