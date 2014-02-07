8aacf6c2b3515b3691f273858fdde07f53a0b3f6
David Hudson <im@davidhudson.me>
2014-02-06 20:23:01 -0600

    Adding routes. Sorry arlton.

---
+++ app/routes.js
@@ -0,0 +1,41 @@
+var stripe;
+
+stripe = require("stripe")(process.env.STRIPE_API_KEY);
+
+module.exports = (function() {
+  var express, app;
+  
+  express = require('express');
+  app = express();
+
+  app.use(express.bodyParser());
+
+  app.get('/api/events', function(req, res) {
+    // Get all events from database
+  });
+  
+  app.get('/api/event/:event', function(req, res) {
+    // Get single event from database
+  });
+
+  app.post('/api/order', function(req, res) {
+    res.writeHead(200, {'content-type':'text/html'});
+
+    // Validate cart
+    
+    // Validate CC info
+    
+    // Process payment
+    
+
+    // Add payment to main object
+    
+    // Update database
+    
+    // Show response JSON
+    res.write(JSON.stringify(req.body));
+    res.end();
+  });
+
+  return app;
+})();
